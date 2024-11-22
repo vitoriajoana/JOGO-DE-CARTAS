d01ffcc69d2df62c4d8bac296128895537de78a0
vitoriajoana <vitoria.soares123459@gmail.com>
2024-11-22 14:47:45 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
-    'Matemática',
+    '★Matemática★',
     'Qual é a fórmula da área de um círculo?',
     'A = πr², onde r é o raio do círculo.'
 )
 
 criaCartao(
-    'Português',
+    '★Português★',
     'O que é uma metáfora?',
     'Uma metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos sem usar conectivos comparativos como "como" ou "parecido com".'
 )
 
 criaCartao(
-    'História',
+    '★História★',
     'Em que ano ocorreu a Proclamação da República no Brasil?',
     'A Proclamação da República ocorreu em 15 de novembro de 1889.'
 )
 
 criaCartao(
-    'Geografia',
+    '★Geografia★',
     'O que são placas tectônicas?',
     'Placas tectônicas são grandes blocos que formam a camada externa sólida da Terra (litosfera) e que se movem sobre o manto terrestre.'
 )
 criaCartao(
-    'Biologia',
+    '★Biologia★',
     'Qual é a função das mitocôndrias nas células?',
     'As mitocôndrias são responsáveis pela produção de energia na forma de ATP por meio da respiração celular.'
 )
 criaCartao(
-    'Física',
+    '★Física★',
     'Qual é a fórmula para calcular a velocidade média?',
     'Velocidade média (v) = distância total percorrida (d) ÷ tempo total (t).'
 )
