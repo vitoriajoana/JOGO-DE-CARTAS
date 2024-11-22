7a2487d3a19524b9d6078aded185aea1d5217091
vitoriajoana <vitoria.soares123459@gmail.com>
2024-11-22 14:35:46 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,33 @@
 criaCartao(
-    'BIOLOGIA',
-    'Como as plantas realizam a fotossíntese?',
-    'As plantas convertem luz solar, água e CO₂ em glicose e oxigênio através da fotossíntese. Isso ocorre nos cloroplastos das células das folhas.'
+    'Matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'A = πr², onde r é o raio do círculo.'
 )
 
 criaCartao(
-    'BIOLOGIA',
-    'Qual a diferença entre mitose e meiose?',
-    'A mitose gera duas células idênticas com o mesmo número de cromossomos, enquanto a meiose gera quatro células com metade dos cromossomos, importantes para a reprodução sexual.'
+    'Português',
+    'O que é uma metáfora?',
+    'Uma metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos sem usar conectivos comparativos como "como" ou "parecido com".'
 )
 
 criaCartao(
-    'BIOLOGIA',
-    'O que é a teoria da evolução de Darwin?',
-    'Darwin propôs que as espécies evoluem através da seleção natural, onde os organismos mais adaptados ao ambiente sobrevivem e se reproduzem, transmitindo suas características.'
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    'A Proclamação da República ocorreu em 15 de novembro de 1889.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'O que são placas tectônicas?',
+    'Placas tectônicas são grandes blocos que formam a camada externa sólida da Terra (litosfera) e que se movem sobre o manto terrestre.'
+)
+criaCartao(
+    'Biologia',
+    'Qual é a função das mitocôndrias nas células?',
+    'As mitocôndrias são responsáveis pela produção de energia na forma de ATP por meio da respiração celular.'
+)
+criaCartao(
+    'Física',
+    'Qual é a fórmula para calcular a velocidade média?',
+    'Velocidade média (v) = distância total percorrida (d) ÷ tempo total (t).'
 )
